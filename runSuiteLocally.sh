e584ff7b097b0804045f1e325449520fe356bcb8
Andy Boyle <andy@sudostream.io>
2017-09-01 19:49:56 +0100

    Can now deploy suite almost totally locally using minikube etc. One exception is creating local Topics using GCloud

--- runSuiteLocally.sh
+++ runSuiteLocally.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function cleanup {
+    echo "Cleanup ..."
+
+    gcloud beta pubsub topics delete UI_REQUEST_TOPIC_LOCAL
+
+    mongod_pid=`ps -ef | grep mongod | grep dbpath | grep timetoteach | grep -v grep | awk '{print $2}'`
+    kill -9 ${mongod_pid}
+
+    minikube stop
+}
+
+
 ORG_DIR=`pwd`
 TEMP_DIR=`mktemp -d` && cd $TEMP_DIR
 
@@ -9,28 +21,29 @@ if [ $? -ne 0 ]; then
     echo
     echo "ERROR: Starting minikube had an issue."
     echo
+    cleanup
     exit 1
 fi
 
 
 echo "Create the Google Pub Sub Topics Locally"
-# TODO
-ls /thiswillfail
+gcloud beta pubsub topics create UI_REQUEST_TOPIC_LOCAL
 if [ $? -ne 0 ]; then
     echo
     echo "ERROR: Attempting to create the Google Pubsub topics failed."
     echo
+    cleanup
     exit 1
 fi
 
 
 echo "Start Mongo DB"
-# TODO
-ls /thiswillfail
+nohup mongod --dbpath /data/mongodb/timetoteach &
 if [ $? -ne 0 ]; then
     echo
     echo "ERROR: Attempting to create the Mongo DB failed. Please check the output above."
     echo
+    cleanup
     exit 1
 fi
 
@@ -38,11 +51,12 @@ fi
 echo "Deploy locally to Kubernetes..."
 git clone git@github.com:SudoStream/devops_k8s.git
 cd devops_k8s
-./deployServiceToKubernetes.sh "local"
+./deployServiceToKubernetes.sh --service="timetoteach-ui-server" --type="local"
 if [ $? -ne 0 ]; then
     echo
     echo "ERROR: Attempting to create the Google Pubsub topics failed."
     echo
+    cleanup
     exit 1
 fi
 
@@ -50,3 +64,4 @@ fi
 ### Cleanup
 cd $ORG_DIR
 rm -rf $TEMP_DIR
+
