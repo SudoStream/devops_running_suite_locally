48aff347db9b17b90ce65477bc01e5b82679a559
Andy Boyle <andy@sudostream.io>
2017-09-12 16:18:14 +0100

    stop kafka

--- functions.sh
+++ functions.sh
@@ -3,10 +3,16 @@
 function cleanup {
     echo "Cleanup ..."
 
-    gcloud beta pubsub topics delete UI_REQUEST_TOPIC_LOCAL
+    #gcloud beta pubsub topics delete UI_REQUEST_TOPIC_LOCAL
 
     mongod_pid=`ps -ef | grep mongod | grep dbpath | grep timetoteach | grep -v grep | awk '{print $2}'`
     kill -9 ${mongod_pid}
+
+    kafka_server_pid=`ps -ef | grep -i kafka | grep zookeeper | grep 'config/server.properties' | awk ' { print $2}' `
+    kill -9 ${kafka_server_pid}
+
+    zookeeper_pid=`ps -ef | grep -i kafka | grep zookeeper | grep 'config/zookeeper.properties' | awk ' { print $2}'`
+    kill -9 ${zookeeper_pid}
 
     minikube stop
 }
